08948dc3db8aa80c4c956d3e737c19a4524acb64
Adam Luchini and Julia Dickey <adam@>
2016-06-13 16:06:08 -0700

    Make the list elements removeable on click

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,49 @@ $(document).ready(function() {
 
   $(".toDoForm").submit(function(event) {
     event.preventDefault();
-    debugger;
+
     var todoItem = $("input#userInput").val();
 
 // "todoItem" below has to match the object key on line 2
     var newToDoList = new ToDoList(todoItem);
 
 // "todoItem" below has to match the object key on line 2
-  $("#userList").append("<li>" + newToDoList.todoItem + "</li>");
+  $("#userList").append("<li>" + newToDoList.todoItem + " " + "<span class='completed-button glyphicon glyphicon-ok'></span>" + "</li>");
+
+    $(".completed-button").last().click(function(){
+      $(this).parent().toggleClass("strikethrough");
+// REPLACE LINE 25 WITH 27 TO REMOVE THE ITEM INSTEAD OF strikethrough
+      // $(this).parent().remove();
+
+      });
+    // });
 
   });
-});
+  });
+
+// });
+
+
+//TANGENT EXPLORING A SOLUTION
+//   function jAdd() {
+//     $("#userList").append("<li><input type='checkbox'>" + newToDoList.todoItem + "</li>");
+//   }
+//   jAdd(newToDoList.todoItem);
+//
+
+//
+//     function jRem() {
+//       $("#userList").children(function () {
+//           return this.checked;
+//       }) .remove();
+//   }
+// jRem(newToDoList.todoItem);
+
+
+
+
+  //
+  // $("#buttonItem").submit(function(event){
+  //   debugger;
+    // $(this).empty();
+    // $("ul#thisTask").empty();
